ff5a9f1bdb07969111a9e9dc175b36c7eedac76c
faizitguy <faizkhan3597@gmail.com>
2021-04-04 12:58:13 +0530

    project-setup

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
-
+const { v4: uuidv4 } = require("uuid");
 const server = require("http").Server(app);
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.status(200).send("Hello World");
+  res.render("room");
 });
 
 server.listen(5000, () => {
